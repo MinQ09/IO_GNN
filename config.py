275d75c5646af9f186f3039af16903e7279cdb3a
Mingyu Kim <kmingyu12@kaist.ac.kr>
2025-07-26 05:34:14 +0900

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
 
     # ────────── Training ──────────
     batch_size:   int   = 8
-    epochs:       int   = 150
+    epochs:       int   = 300
     lr:           float = 5e-4
     weight_decay: float = 1e-4
     patience:     int   = 50             
